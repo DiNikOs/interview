2068fe72e777eb3aa726884660a78ef78d1e1a8a
DiNikOs <demon22odn08@rambler.ru>
2020-05-10 20:33:04 +0300

    HW6. Java web TomCat

---
+++ hw6_java_web_module/src/main/java/ru/geekbrains/Main.java
@@ -0,0 +1,26 @@
+/**
+ * @author Ostrovskiy Dmitriy
+ * @created 10.05.2020
+ * Main class
+ * @version v1.0
+ */
+
+package ru.geekbrains;
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
